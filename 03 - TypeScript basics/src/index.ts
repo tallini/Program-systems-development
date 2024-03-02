a45b3f11c9c9d52421629484549c0196ef679a45
Zoltan R. Janki <jankiz@inf.u-szeged.hu>
2024-03-02 22:04:05 +0100

    03 and 04 practice codes updated.

--- 03 - TypeScript basics/src/index.ts
+++ 03 - TypeScript basics/src/index.ts
@@ -1,15 +1,22 @@
-import { MainClass } from './main_class';
+import { MainClass } from './main-class';
 import express from 'express';
+import { Request, Response } from 'express';
+
+// let vs. var vs. const
+// any vs. unknown
+// undefined vs. null
+// === vs. ==
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.get('/', (req, res) => {
-	let myClass = new MainClass();
-	res.status(200).send(myClass.greeting());
-});
+
+app.get('/', (req: Request, res: Response) => {
+    let myClass = new MainClass();
+    res.status(200).send('Hello, World!');
+})
 
 app.listen(port, () => {
-	console.log('App is listening on port 3000...');
+    console.log('Server is listening on port ' + port.toString());
 });
 
--- 04 - Asynchronous operations/src/index.ts
+++ 04 - Asynchronous operations/src/index.ts
@@ -1,52 +1,86 @@
-import { MainClass } from './main_class';
+import { MainClass } from './main-class';
 import express from 'express';
+import { Request, Response } from 'express';
+import { lastValueFrom, take } from 'rxjs';
+
+// let vs. var vs. const
+// any vs. unknown
+// undefined vs. null
+// === vs. ==
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-app.get('/', (req, res) => {
-	res.status(404).send('This is not an available route, please try /promise or /observable.');
-})
 
-app.get('/promise', async (req, res) => {
-	let myClass = new MainClass();
-	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-	res.setHeader('Transfer-Encoding', 'chunked');
-	res.write('Waiting for the result from Promise...\n');
-	myClass.greetingPromise().then((data: string) => {
-		res.write(data);
-		res.status(200).end();	
-	}).catch((error: string) => {
-		res.write(error);
-		res.status(404).end();
-	});
+app.get('/', (req: Request, res: Response) => {
+    let myClass = new MainClass();
+    res.status(200).send('Hello, World!');
+});
 
-	/*
-	// async-await (same as Promise above)
-    try {
-        const data = await myClass.greetingPromise();
+app.get('/callback', (req: Request, res: Response) => {
+    let myClass = new MainClass();
+    myClass.monitoringCallback((error, result) => {
+        if (error) {
+            res.write(error);
+            res.status(400).end();
+        } else {
+            res.write(result);
+            res.status(200).end();
+        }
+    });
+});
+
+app.get('/promise', async (req: Request, res: Response) => {
+    let myClass = new MainClass();
+    /* myClass.monitoringPromise().then((data: string) => {
         res.write(data);
         res.status(200).end();
-    } catch (error: any) {
+    }).catch((error: string) => {
         res.write(error);
-        res.status(404).end();
-    } */
+        res.status(400).end();
+    }); */
+
+
+    // async-await
+    try {
+        const data = await myClass.monitoringPromise();
+        res.write(data);
+        res.status(200).end();
+    } catch(error) {
+        res.write(error);
+        res.status(400).end();
+    }
 });
 
-app.get('/observable', (req, res) => {
-	let myClass = new MainClass();
-	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-	res.setHeader('Transfer-Encoding', 'chunked');
-	myClass.greetingObservable().subscribe((data: string) => {
-		res.write(data);	
-	}, (error: string) => {
-		res.status(404).end(error);
-	}, () => {
-		res.status(200).end();
-	});
-});
+
+app.get('/observable', (req: Request, res: Response) => {
+    let myClass = new MainClass();
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    res.setHeader('Transfer-Encoding', 'chunked');
+    
+    // deprecated variant
+    /* myClass.monitoringObservable().subscribe((data) => {
+        console.log(data);
+    }, (error) => {
+        console.log(error);
+    }, () => {
+        console.log('complete');
+    }); */
+
+    myClass.monitoringObservable().subscribe({
+        next(data: string) {
+            res.write(data);
+        }, error(error: string) {
+            res.status(400).end(error);
+        }, complete() {
+            res.status(200).end();
+        }
+    });
+})
 
 app.listen(port, () => {
-	console.log('App is listening on port 3000...');
+    console.log('Server is listening on port ' + port.toString());
 });
+
+console.log('After server is ready.');
 
