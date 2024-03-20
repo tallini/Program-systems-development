3f299b50aef63c9c6c76f8cfa85b61d9069472a4
Inez <kalman.inez2000@gmail.com>
2024-03-20 14:50:43 +0100

    05-Middlewares style changes

--- 05 - Middlewares/src/passport/passport.ts
+++ 05 - Middlewares/src/passport/passport.ts
@@ -3,7 +3,6 @@ import { Strategy } from 'passport-local';
 import { User } from '../model/User';
 
 export const configurePassport = (passport: PassportStatic): PassportStatic => {
-
     passport.serializeUser((user: Express.User, done) => {
         console.log('user is serialized.');
         done(null, user);
@@ -14,13 +13,16 @@ export const configurePassport = (passport: PassportStatic): PassportStatic => {
         done(null, user);
     });
 
-    passport.use('local', new Strategy((username, password, done) => {
-        if (username === 'test@test.com' && password === 'testpw') {
-            done(null, new User(username, password));
-        } else {
-            done('Incorrect username or password.');
-        }
-    }));
+    passport.use(
+        'local',
+        new Strategy((username, password, done) => {
+            if (username === 'test@test.com' && password === 'testpw') {
+                done(null, new User(username, password));
+            } else {
+                done('Incorrect username or password.');
+            }
+        })
+    );
 
     return passport;
-}
+};
